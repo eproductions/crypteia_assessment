5d771f6d3870bef638ea1fe5729cdf6fe68f6e08
Elias Papadamos <epapadamos@gmail.com>
2021-05-31 16:05:54 +0300

    problem 4 solution

--- index.js
+++ index.js
@@ -25,7 +25,16 @@ exports.sumDeep = (list) => {
   }, { objects: 0 }));
 };
 
-exports.applyStatusColor = () => {};
+exports.applyStatusColor = (colors, statuses) => {
+  return statuses.map(item => {
+    let color = Object.keys(colors).find(key => colors[key].includes(item.status));
+    if ( color ) {
+      item['color'] = color;
+      return item;
+    }
+  }).filter(item => item !== undefined);
+};
+
 exports.createGreeting = () => {};
 exports.setDefaults = () => {};
 exports.fetchUserByNameAndUsersCompany = () => {};
